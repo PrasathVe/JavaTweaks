24b619d07efe00ca797747a3d01e188b9cc68db9
Prasath <vijayst08@gmail.com>
2023-08-24 19:31:37 +0530

    Lambda

--- LambdaEx.java
+++ LambdaEx.java
@@ -1,10 +1,25 @@
 package com.java.java20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LambdaEx {
+	public void usingArrayList() {
+		List<String> list = new ArrayList<>();
+		
+		list.add("Horse");
+		list.add("Tiger");
+		list.add("Lion");
+		
+		list.forEach((e) -> {System.out.print(e + " ");
+		
+		});
+}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");	
+		LambdaEx lamb = new LambdaEx();
+		lamb.usingArrayList();
 
 	}
 
